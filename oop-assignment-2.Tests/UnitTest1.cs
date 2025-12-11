314218a3bc98e09652a0fb718079d6179a528055
Raheem <folabello27@gmail.com>
2025-12-11 18:00:20 +0000

    71757

---
+++ oop-assignment-2.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace oop_assignment_2.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ oop-assignment-2.Tests/UnitTestQuestion_1.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using oop_assignment_2_2025_71757.Models;
+using Assert = NUnit.Framework.Assert;
+
+namespace oop_assignment_2.Tests
+{
+    public class UnitTestQuestion_1
+    {
+        [Fact]
+        public void IsValidIrishMobile_ValidInput_ReturnsTrue()
+        {
+            var q1 = new ExamQuestion_1();
+            Assert.True(q1.IsValidIrishMobile("0831234567"));
+            Assert.True(q1.IsValidIrishMobile("0859876543"));
+            Assert.True(q1.IsValidIrishMobile("0891122334"));
+        }
+
+        [Fact]
+        public void IsValidIrishMobile_InvalidInput_ReturnsFalse()
+        {
+            var q1 = new ExamQuestion_1();
+            Assert.False(q1.IsValidIrishMobile("0812345678")); 
+            Assert.False(q1.IsValidIrishMobile("083 1234567")); 
+            Assert.False(q1.IsValidIrishMobile("08312345678"));
+        }
+
+        [Fact]
+        public void CalculateDiscount_ReturnsCorrectPercentage()
+        {
+            var q1 = new ExamQuestion_1();
+            Xunit.Assert.Equal(0.05, q1.CalculateDiscount(1));
+            Xunit.Assert.Equal(0.10, q1.CalculateDiscount(2));
+            Xunit.Assert.Equal(0.0, q1.CalculateDiscount(0));
+        }
+    }
+}
