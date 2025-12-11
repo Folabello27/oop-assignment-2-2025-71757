c0c567cfce846c9c9e1a84783e772b2cbf86bd71
Raheem <folabello27@gmail.com>
2025-12-11 17:57:11 +0000

    Delete oop-assignment-2-2025-71757 directory

--- oop-assignment-2-2025-71757/oop-assignment-2-2025-71757/Models/ExamQuestion_5.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace oop_assignment_2_2025_71757.Models;
-
-public class ExamQuestion_5
-{
-    
-}
--- oop-assignment-2-2025-71757/oop-assignment-2.Tests/UnitTestQuestion_5.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace oop_assignment_2.Tests;
-
-public class UnitTestQuestion_5
-{
-    
-}
